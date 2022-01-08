d73d1afd7ab09aba0d0822712a57632e0baa0f2b
KieranHannagan <kieranhan1999@gmail.com>
2022-01-08 15:35:38 -0600

    updated get routes to join tables based on id

--- server.js
+++ server.js
@@ -25,7 +25,12 @@ const db = mysql.createConnection(
 
 // Get a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name 
+                AS party_name 
+                FROM candidates 
+                LEFT JOIN parties 
+                ON candidates.party_id = parties.id`;
+
     const params = [req.params.id];
   
     db.query(sql, params, (err, row) => {
@@ -43,7 +48,12 @@ app.get('/api/candidate/:id', (req, res) => {
 
 // Delete a candidate
 app.delete('/api/candidate/:id', (req,res) => {
-    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name 
+                AS party_name 
+                FROM candidates 
+                LEFT JOIN parties 
+                ON candidates.party_id = parties.id 
+                WHERE candidates.id = ?`;
     const params = [req.params.id];
 
     db.query(sql, params, (err, result) => {
@@ -86,7 +96,12 @@ app.post('/api/candidate', ({ body }, res) => {
 
 // Get all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name 
+                AS party_name 
+                FROM candidates 
+                LEFT JOIN parties 
+                ON candidates.party_id = parties.id`;
+
   
     db.query(sql, (err, rows) => {
       if (err) {
@@ -110,3 +125,4 @@ app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
 
+// module 12.3.5
